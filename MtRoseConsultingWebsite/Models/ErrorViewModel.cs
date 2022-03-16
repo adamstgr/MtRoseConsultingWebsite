5ac55f2de2c414518a3e9179ce3855b6946dd224
andrew <andrewjharris96@gmail.com>
2022-03-16 19:03:48 -0400

    generate project

---
+++ MtRoseConsultingWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MtRoseConsultingWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
